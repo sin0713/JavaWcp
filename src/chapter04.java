0c6be83385f3ce8fd474ae5680ef10e406ad7ad8
sin0713 <narisin1124@gmail.com>
2021-12-28 23:17:38 +0900

    [Add]chapter4完了

---
+++ src/chapter04.java
@@ -0,0 +1,12 @@
+
+public class chapter04 {
+	public static void main(String[] args) {
+		String word = "成田進之介";
+		final int LUCKY_NUMBER = 7;
+		System.out.println(word);
+		
+		word = "自分を信じないやつに努力する価値はない";
+		System.out.println(word);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
