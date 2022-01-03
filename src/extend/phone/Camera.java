e94314f6470fba43c7740b0f8fda9f8e90f29411
sin0713 <narisin1124@gmail.com>
2022-01-03 22:37:42 +0900

    [Add]chapter13完了

---
+++ src/extend/phone/Camera.java
@@ -0,0 +1,6 @@
+package extend.phone;
+
+public interface Camera {
+	void takePicture();
+	
+}
--- src/practice/CapsuleMan.java
+++ src/practice/CapsuleMan.java
@@ -4,22 +4,20 @@ public class CapsuleMan {
 	 private String name;
 	 private int age;
 
-	    public CapsuleMan(String name, int age) {
-	        this.name = name;
-	        this.age = age;
-	    }
+	 public CapsuleMan(String name, int age) {
+		 this.name = name;
+	     this.age = age;
+	 }
 
+	 public int getAge() {
+	     return this.age;
+	 }
 
+	 public String getName() {
+	     return this.name;
+	 }
 
-	    public int getAge() {
-	    	return this.age;
-	    }
-
-	    public String getName() {
-	    	return this.name;
-	    }
-
-	    public void setAge(int age) {
-	    	this.age = age;
-	    }
+	 public void setAge(int age) {
+	     this.age = age;
+	 }
 }
